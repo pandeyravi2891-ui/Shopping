623af58d35045609ad3f65e87283a7da27cd2d14
pandeyravi2891-ui <pandeyravi2891@gmail.com>
2026-08-22 20:42:16 +0530

    feat: implement MongoDB database connection configuration using Mongoose

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
-// require("dotenv").config();
+require("dotenv").config();
 
 
 async function connect() {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/portfolio");
+        await mongoose.connect(process.env.MONGODB_URL);
         console.log("Database Connected");
     } catch (err) {
         console.log(err);
     }
 }
 
-module.exports=connect;
+module.exports = connect;
 
 
